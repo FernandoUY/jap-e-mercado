472ba95aff6d89dc115468868a9177449d707e5b
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2023-09-12 20:17:25 -0300

    refactor: switch fetch to getJSONData and update html

--- js/product-info.js
+++ js/product-info.js
@@ -1,28 +1,43 @@
 // Buscamos la id del producto en localStorage
 const productID = localStorage.getItem("productId");
 
-  // Cuando carga la página asignamos la respuesta de los comentarios en una variable y mostramos los comentarios
-  document.addEventListener("DOMContentLoaded", async function() {
-    const comments = await fetchProductComments(productID);
-    console.log(comments)
-    showComments(comments)
-  })
+// Cuando carga la página asignamos la respuesta de los comentarios en una variable y mostramos los comentarios
+document.addEventListener("DOMContentLoaded", async function () {
+  const productComments = await fetchProductComments(productID);
+  const productInfo = await fetchProductInfo(productID)
+  showProductComments(productComments);
+  showProductInfo(productInfo)
+});
+
+// Función para obtener los detalles de un producti por su ID
+async function fetchProductInfo(productID) {
+  const response = await getJSONData(`${PRODUCT_INFO_URL}${productID}.json`);
+  if (response.status === "ok") {
+    return response.data;
+  }
+}
 
 // Función para obtener los comentarios de un producto por su ID
 async function fetchProductComments(productID) {
-  const response = await getJSONData(`${PRODUCT_INFO_COMMENTS_URL}${productID}.json`);
+  const response = await getJSONData(
+    `${PRODUCT_INFO_COMMENTS_URL}${productID}.json`
+  );
   if (response.status === "ok") {
     return response.data;
   }
 }
 
 //Mostrar los coemntarios en pantalla (de momento sin las estrellitas :(  )
-function showComments(comments){
-  const divCom = document.getElementById("comm");
+function showProductComments(comments) {
+  const commentsSectionDiv = document.getElementById("comments-section");
 
-  comments.forEach(comment => {
-    divCom.innerHTML += 
-    `
+  if (!comments.length) {
+    commentsSectionDiv.innerHTML += `
+      <h4 class="text-muted">Aún no hay comentarios sobre este producto</h4>
+    `;
+  }
+  comments.forEach((comment) => {
+    commentsSectionDiv.innerHTML += `
     <div class="list-group list-group-item-action flex-colum align-items-start">
       <div class="list-group-item list-group-item-action flex-column align-items-start">
         <div class="d-flex w-100 justify-content-between">
@@ -33,8 +48,18 @@ function showComments(comments){
         <small>${comment.dateTime}</small>
       </div>
     </div>
-    `
+    `;
   });
+}
 
+function showProductInfo(product) {
+  const infoSectionDiv = document.getElementById("info-section");
 
+  infoSectionDiv.innerHTML = `
+    <h1>${product.name}</h1>
+    <p>${product.description}</p>
+    <p>${product.currency} ${product.cost}</p>
+    <p>${product.category}</p>
+    <p>${product.soldCount} vendidos</p>
+  `
 }
