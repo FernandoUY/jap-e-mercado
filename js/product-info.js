47f948cf67c73a8cd6e90178e0bcffb1e1489380
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2023-09-12 17:24:15 -0300

    fix: comments

--- js/product-info.js
+++ js/product-info.js
@@ -4,16 +4,16 @@ const productID = localStorage.getItem("productId");
   // Cuando carga la página asignamos la respuesta de los comentarios en una variable y mostramos los comentarios
   document.addEventListener("DOMContentLoaded", async function() {
     const comments = await fetchProductComments(productID);
+    console.log(comments)
     showComments(comments)
   })
 
 // Función para obtener los comentarios de un producto por su ID
-function fetchProductComments(productID) {
-  return getJSONData(`${PRODUCT_INFO_COMMENTS_URL}${productID}.json`).then(response => {
-    if (response.status === "ok") {
-      return response.data
-    }
-  })
+async function fetchProductComments(productID) {
+  const response = await getJSONData(`${PRODUCT_INFO_COMMENTS_URL}${productID}.json`);
+  if (response.status === "ok") {
+    return response.data;
+  }
 }
 
 //Mostrar los coemntarios en pantalla (de momento sin las estrellitas :(  )
