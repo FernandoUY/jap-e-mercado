5a313f3bfedae493460fcd73c7706e79368b77ab
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2023-10-24 17:22:44 -0300

    fix credit card validation

--- js/ValidacionesCart.js
+++ js/ValidacionesCart.js
@@ -1,72 +1,88 @@
-
 // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
-    var forms = document.querySelectorAll('.needs-validation')      
+var forms = document.querySelectorAll(".needs-validation");
 
-      forms.forEach(element => {
-        element.addEventListener('submit', function (event) {
-            element.classList.add('was-validated')
-            event.preventDefault()
-            ValidarTipoDeEnvio()
-            Val_DatosEnvio()
+forms.forEach((element) => {
+  element.addEventListener("submit", function (event) {
+    element.classList.add("was-validated");
+    event.preventDefault();
+    ValidarTipoDeEnvio();
+    Val_DatosEnvio();
 
-//verificamos la validacion para mostrar aviso de compra exitosa
-            if (element.checkValidity()) {
-                if (ValidarTipoDeEnvio()) {
-                
-                    var avisoCompraExitosa = document.getElementById("aviso-compra-exitosa");
-                    avisoCompraExitosa.style.display = "block";
-
-                }
-            }
-        })
-      });
+    //verificamos la validacion para mostrar aviso de compra exitosa
+    if (element.checkValidity()) {
+      if (ValidarTipoDeEnvio()) {
+        var avisoCompraExitosa = document.getElementById(
+          "aviso-compra-exitosa"
+        );
+        avisoCompraExitosa.style.display = "block";
+      }
+    }
+  });
+});
 
 // Funcion que busca si algun tipo de pago
 
 function ValidarTipoDeEnvio() {
-    let paymentMethodRadios = document.querySelectorAll('input[name="paymentMethod"]');
-    var nometodopago = document.getElementById("P-nometodopago")
-    var BtnmetodPago = document.getElementById("BtnmetodPago")
+  let paymentMethodRadios = document.querySelectorAll(
+    'input[name="paymentMethod"]'
+  );
+  var nometodopago = document.getElementById("P-nometodopago");
+  var BtnmetodPago = document.getElementById("BtnmetodPago");
 
-    if((!(paymentMethodRadios[0].checked)) && (!(paymentMethodRadios[1].checked))){
-        
-        nometodopago.classList.remove("invisible")
-        BtnmetodPago.classList.add("text-danger")
-        return false; 
-    }
-    else if((!(paymentMethodRadios[0].checked)) || (!(paymentMethodRadios[1].checked))){
+  if (!paymentMethodRadios[0].checked && !paymentMethodRadios[1].checked) {
+    nometodopago.classList.remove("invisible");
+    BtnmetodPago.classList.add("text-danger");
+    return false;
+  } else if (
+    !paymentMethodRadios[0].checked ||
+    !paymentMethodRadios[1].checked
+  ) {
+    nometodopago.classList.add("invisible");
+    BtnmetodPago.classList.remove("text-danger");
+  }
 
-        nometodopago.classList.add("invisible")
-        BtnmetodPago.classList.remove("text-danger")
-
-    }
-
-    return true;
+  return true;
 }
 
 //funcion para avisar que faltan datos en el tipo de pago
-function Val_DatosEnvio(){
-    let paymentMethodRadios = document.querySelectorAll('input[name="paymentMethod"]');
-    let cardNumberInput = document.getElementById('cardNumber');
-    let securityCodeInput = document.getElementById('securityCode');
-    let expirationDateInput = document.getElementById('expirationDate');
-    let accountNumberInput = document.getElementById('accountNumber');
-    let metodPago = document.getElementById("MetodPago")
+function Val_DatosEnvio() {
+  let paymentMethodRadios = document.querySelectorAll(
+    'input[name="paymentMethod"]'
+  );
+  let cardNumberInput = document.getElementById("cardNumber");
+  let securityCodeInput = document.getElementById("securityCode");
+  let expirationDateInput = document.getElementById("expirationDate");
+  let accountNumberInput = document.getElementById("accountNumber");
+  let metodPago = document.getElementById("MetodPago");
+
+  if (paymentMethodRadios[0].checked) {
+    const validCreditCardRegexp = /(\d{4}[-. ]?){4}|\d{4}[-. ]?\d{6}[-. ]?\d{5}/g
     
-    if(paymentMethodRadios[0].checked){
-        if(cardNumberInput.value === "" && securityCodeInput.value === "" && expirationDateInput.value === ""){
-            metodPago.innerHTML += `<div class="alert alert-danger">
-            Faltan datos de tu tarjeta
-          </div>`
-        }
+    if (!validCreditCardRegexp.test(cardNumberInput.value)) {
+      metodPago.innerHTML += `
+      <div class="alert alert-danger">
+        Debe escribir un numero de taFaltan datos de tu tarjetarjeta correcto (16 numeros en total separados por - o espacios)
+      </div>`;
     }
 
-    if (paymentMethodRadios[1].checked) {
-        if (accountNumberInput.value === "") {
-            metodPago.innerHTML += `<div class="alert alert-danger">
-            Faltan datos de tu banco
-          </div>`
-        }
+    if (
+      cardNumberInput.value === "" &&
+      securityCodeInput.value === "" &&
+      expirationDateInput.value === ""
+    ) {
+      metodPago.innerHTML += `
+      <div class="alert alert-danger">
+        Faltan datos de tu tarjeta
+      </div>`;
     }
-    
+  }
+
+  if (paymentMethodRadios[1].checked) {
+    if (accountNumberInput.value === "") {
+      metodPago.innerHTML += `
+      <div class="alert alert-danger">
+        Faltan datos de tu banco
+      </div>`;
+    }
+  }
 }
