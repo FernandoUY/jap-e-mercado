38bf0cce8e7913e9cff8a44701f2cef8bb542feb
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2023-10-31 19:41:37 -0300

    my-profile design created and refactor login validation

--- js/login.js
+++ js/login.js
@@ -1,18 +1,14 @@
-const random = () => {
-  return Math.random().toString(36).substring(2);
-};
-
-const token = () => {
-  return random() + random();
-};
-
 document.addEventListener("submit", function () {
-  const emailValue = document.getElementById("emailInput").value //se obtiene el valor del email
+  const userEmail = document.getElementById("emailInput").value;
 
-  let emailValueUser = emailValue.slice(0, -10)
+  const userDetails = {
+    firstName: null,
+    secondName: null,
+    firstLastname: null,
+    secondLastname: null,
+    email: userEmail,
+    phone: null,
+  };
 
-  localStorage.setItem("token", token());
-  localStorage.setItem("user", emailValueUser);
+  localStorage.setItem("userDetails", JSON.stringify(userDetails));
 });
-
-
