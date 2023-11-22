715d0c671ec594ea1285d7b5865ebd1baee1b336
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2023-11-22 17:14:02 -0300

    api nuevo

--- index.js
+++ index.js
@@ -1,25 +1,64 @@
 // Imports
 const express = require("express");
-const cors = require("cors");
 const handleErrors = require("./middlewares/handleError");
 
 // Express initialization
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 // Body parser
 app.use(express.json());
 
-// CORS
-app.use(cors())
+// Url parser
+app.use(express.urlencoded({ extended: true }));
 
-// Routes
+// Static files
+app.use(express.static("public"));
+
+// API Routes
 app.use("/emercado-api", require("./routes/emercadoRoutes"));
+
+// Pages Routes
+app.get("/", (req, res) => {
+  res.sendFile("D:/emercado-api/pages/index.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile("D:/emercado-api/pages/login.html");
+});
+
+app.get("/categories", (req, res) => {
+  res.sendFile("D:/emercado-api/pages/categories.html");
+});
+
+app.get("/my-profile", (req, res) => {
+  res.sendFile("D:/emercado-api/pages/my-profile.html");
+});
+
+app.get("/cart", (req, res) => {
+  res.sendFile("D:/emercado-api/pages/cart.html");
+});
+
+app.get("/signout", (req, res) => {
+  res.sendFile("D:/emercado-api/pages/signout.html");
+});
+
+app.get("/products", (req, res) => {
+  res.sendFile("D:/emercado-api/pages/products.html");
+});
+
+app.get("/product-info", (req, res) => {
+  res.sendFile("D:/emercado-api/pages/product-info.html");
+});
+
+app.get("/sell", (req, res) => {
+  res.sendFile("D:/emercado-api/pages/sell.html");
+});
 
 // Error handler
 app.use(handleErrors);
 
 // Server listener
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
